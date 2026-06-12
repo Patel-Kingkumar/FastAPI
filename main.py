41affc4d4ac1886be8e9fe50ca687c502a6dde82
King Patel <kingpatel0409@gmail.com>
2026-06-12 12:20:54 +0530

    connect database

--- main.py
+++ main.py
@@ -1,109 +1,40 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from fastapi import FastAPI, Depends 
+ 
 app = FastAPI()
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args = {"check_same_thread":False}
+)
+
+sessionLocal = sessionmaker(bind = engine)
+
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
     
-#home route
-# @app.get("/")
-# def home():
-#     return {"message": "Hello, World!"}
-
-#about route
-# @app.get("/about")
-# def about():
-#     return {"message": "This is a simple FastAPI application."}
-
-#users route
-# @app.get("/users")
-# def users():
-#     return [{"id": 1, "name": "Alice"}, {"id": 2, "name": "Bob"}]
-
-#get by id route
-# @app.get("/users/{user_id}")
-# def get_user(user_id: int):
-#     return {"id": user_id, "name": f"User {user_id}"}
-
-#query params
-# @app.get("/users1")
-# def get_user(name: str = None):
-#     return {"name": name}
-
-# @app.post("/create-user")
-# def create_user(name: str, age: int): it's like query paramas
-#     return {"name": name, "age": age}
-
-# def create_user(user: dict):
-#     return {
-#         "message": "User created successfully",
-#         "data": user
-#     }
-
-# schema for user with nested schema
-# class Address(BaseModel):
-#     city: str
-#     state: str
+    id = Column(Integer, primary_key = True, index = True)
+    title = Column(String)
+    completed = Column(String)
     
-# class User(BaseModel):
-#     name: str
-#     age: int
-#     address: Address
+#create table in database -> create test.db file
+Base.metadata.create_all(bind = engine)
 
-# @app.post("/create-user")
-# def create_user(user: User):
-#     return {
-#         "message": "User created successfully",
-#         "data": user
-#     }
 
-#CRUD operations with in-memory storage
-
-todos = []
-
-class Todo(BaseModel):
-    id: int
-    title: str
-    completed: bool
-    
-@app.post("/todos")
-def create_todo(todo: Todo):
-    todos.append(todo)
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@app.get("/")
+def home(db: Session = Depends(get_db)):
     return {
-        "message": "Todo created successfully",
-        "data": todo
+        "message": "DB Connected"
     }
-
-@app.get("/todos")
-def get_todos():
-    return {
-        "message": "Todos retrieved successfully",
-        "data": todos
-    }
-    
-@app.get("/todos/{todo_id}")
-def get_todo(todo_id: int):
-    for todo in todos:
-        if todo.id == todo_id:
-            return {
-                "message": "Todo retrieved successfully",
-                "data": todo
-            }
-    return {"message": "Todo not found"}
-
-@app.put("/todos/{todo_id}")
-def update_todo(todo_id: int, update_todo: Todo):
-    for i, t in enumerate(todos):
-        if t.id == todo_id:
-            todos[i] = update_todo
-            return {
-                "message": "Todo updated successfully",
-                "data": update_todo
-            }
-    return {"message": "Todo not found"}
-
-@app.delete("/todos/{todo_id}")
-def delete_todo(todo_id: int):
-    for i, t in enumerate(todos):
-        if t.id == todo_id:
-            del todos[i]
-            return {"message": "Todo deleted successfully"}
-    return {"message": "Todo not found"}
